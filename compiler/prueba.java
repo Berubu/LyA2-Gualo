53738acd2670c65d031e3f6a47c5e0e7d899088a
BERU <93852000+Berubu@users.noreply.github.com>
2025-09-18 11:46:50 -0600

    Update prueba.java

--- compiler/prueba.java
+++ compiler/prueba.java
@@ -1,9 +1,15 @@
 public class Nombre {
     public int metodoA(int a, double b, int x){
+        double f,g,h;
         x= a+ (h*v)-34*sd
+        int a,b,c;
+        c = b *34 - a;
     }
     private intatribx, y, z;
     public int metodoB (){}
     public double a,b,c;
 
+
 }
+
+
